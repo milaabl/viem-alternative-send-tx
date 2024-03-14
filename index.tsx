97f662adb2c727ecd916c6656a5a1d795ce330f7
Ludmila <milaabl2405@gmail.com>
2024-03-14 19:45:56 +0300

    chore: initial commit, add architecture and scaffold a new viem + TS project

---
+++ index.tsx
@@ -0,0 +1,89 @@
+import React, { useEffect, useState } from 'react';
+import ReactDOM from 'react-dom/client';
+import {
+  http,
+  Address,
+  Hash,
+  TransactionReceipt,
+  createPublicClient,
+  createWalletClient,
+  getContract,
+  custom,
+  stringify,
+} from 'viem';
+import { goerli } from 'viem/chains';
+import 'viem/window';
+import { wagmiContract } from './contract';
+
+const publicClient = createPublicClient({
+  chain: goerli,
+  transport: http(),
+});
+const walletClient = createWalletClient({
+  chain: goerli,
+  transport: custom(window.ethereum!),
+});
+
+function Example() {
+  const [account, setAccount] = useState<Address>();
+  const [hash, setHash] = useState<Hash>();
+  const [receipt, setReceipt] = useState<TransactionReceipt>();
+
+  const connect = async () => {
+    const [address] = await walletClient.requestAddresses();
+    setAccount(address);
+  };
+
+  const mint = async () => {
+    if (!account) return;
+    const wagmiContractInteracteable = getContract({
+      address: wagmiContract.address,
+      abi: wagmiContract.abi,
+      client: publicClient,
+    });
+    const txHash = await wagmiContractInteracteable.write.mint({
+      ...wagmiContract,
+      functionName: 'mint',
+      account,
+    });
+    const receipt = await publicClient.waitForTransactionReceipt({
+      hash: txHash,
+    });
+
+    console.log({ receipt });
+
+    setHash(txHash);
+  };
+
+  useEffect(() => {
+    (async () => {
+      if (hash) {
+        const receipt = await publicClient.waitForTransactionReceipt({ hash });
+        setReceipt(receipt);
+      }
+    })();
+  }, [hash]);
+
+  if (account)
+    return (
+      <>
+        <div>Connected: {account}</div>
+        <button onClick={mint}>Mint</button>
+        {receipt && (
+          <>
+            <div>
+              Receipt:{' '}
+              <pre>
+                <code>{stringify(receipt, null, 2)}</code>
+              </pre>
+            </div>
+          </>
+        )}
+      </>
+    );
+  return <button onClick={connect}>Connect Wallet</button>;
+}
+
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+  <Example />
+);
